dddf828fce912fdd6d5a83c5a39e0fb20d2100e5
Pro100-Adm <kirow.alesha@yandex.ru>
2022-11-12 17:59:17 +0300

    Kirov A.D. seminar 12.11.2022 homework

---
+++ homework/task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int AskUser()
+{
+Console.Write("Введите число: ");
+string Ask = Console.ReadLine();
+try
+{
+    int op = int.Parse(Ask);
+    return op;
+}
+catch (Exception)
+{
+    Console.WriteLine("Введите верное число!");
+    return AskUser();
+}
+}
+
+bool isEven(int op)
+{
+    if (op%2 == 0){
+        Console.WriteLine("True");
+        return true;
+    }
+    else{
+        Console.WriteLine("False");
+        return false;
+    }
+}
+
+
+isEven(AskUser());
