6ef29b1d9f19cc03700af47f3996eebb49a3b701
Pro100-Adm <kirow.alesha@yandex.ru>
2022-11-12 16:42:02 +0300

    Kirov A.D. seminar 12.11.2022 homework

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+double AskUser()
+{
+Console.Write("Введите число: ");
+string Ask = Console.ReadLine();
+try
+{
+    double op = double.Parse(Ask);
+    return op;
+}
+catch (Exception)
+{
+    Console.WriteLine("Введите верное число!");
+    return AskUser();
+}
+}
+
+double Square(double op)
+{
+    double result;
+    result = Math.Pow(op, 2);
+    Console.WriteLine(result);
+    return result;
+}
+
+
+Square(AskUser());
---
+++ homework/task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int AskUser()
+{
+Console.Write("Введите число: ");
+string Ask = Console.ReadLine();
+try
+{
+    int op = int.Parse(Ask);
+    return op;
+}
+catch (Exception)
+{
+    Console.WriteLine("Введите верное число!");
+    return AskUser();
+}
+}
+
+int Max(int op1, int op2, int op3)
+{
+    int max;
+    if (op1>op2){
+        max = op1;
+    }
+    else {
+        max = op2;
+    }
+    if (op3>max){
+        max = op3;
+    }
+    Console.WriteLine($"Max number: {max}");
+    return max;
+}
+
+Max(AskUser(), AskUser(), AskUser());
